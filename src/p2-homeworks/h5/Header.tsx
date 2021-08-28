28acda47583cb0be756a8d4cac41c3b68aa4f46a
Yuliya <julia1997.04.14@mail.ru>
2021-08-28 18:23:56 +0300

    home work 5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import './Header.css'
+
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={"nav"}>
+            <button className={"mainMenuButton"}>nav</button>
+            <div className={"dropdownChild"}>
+                <NavLink to="/pre-junior">preJunior </NavLink>
+                <NavLink to="/junior">Junior </NavLink>
+                <NavLink to="/junior-plus">Junior+</NavLink>
+            </div>
         </div>
     )
 }
-
 export default Header
---
+++ src/p2-homeworks/h5/pages/Junior+.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function JuniorPlus() {
+    return (
+        <div>
+            {/*<HW13/>*/}
+
+        </div>
+    )
+}
+
+export default JuniorPlus
